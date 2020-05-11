c96314d12134f218c6669592cd4ae8933df3049f
Laksono Adhianto <laksono@gmail.com>
2020-05-11 18:04:10 -0500

    remove old hpcdata

--- edu.rice.cs.hpcdata/src/edu/rice/cs/hpcdata/experiment/metric/format/IMetricValueFormat.java
+++
@@ -1,8 +0,0 @@
-package edu.rice.cs.hpcdata.experiment.metric.format;
-
-import edu.rice.cs.hpcdata.experiment.metric.MetricValue;
-
-public interface IMetricValueFormat {
-
-	public String format(MetricValue value);
-}
