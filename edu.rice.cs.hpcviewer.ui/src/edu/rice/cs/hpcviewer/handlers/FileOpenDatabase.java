1c49c57216aab045355a4c0948aeb226506f88c8
Laksono Adhianto <laksono@gmail.com>
2020-05-14 19:05:56 -0500

    populate bottom-up and flat views

--- edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/handlers/FileOpenDatabase.java
+++ edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/handlers/FileOpenDatabase.java
@@ -6,11 +6,15 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.workbench.IWorkbench;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService.PartState;
 import org.eclipse.swt.widgets.Shell;
 
 import edu.rice.cs.hpc.data.experiment.BaseExperiment;
 import edu.rice.cs.hpcviewer.experiment.ExperimentAddOn;
 import edu.rice.cs.hpcviewer.experiment.ExperimentManager;
+import edu.rice.cs.hpcviewer.ui.parts.BottomUpPart;
+import edu.rice.cs.hpcviewer.ui.parts.FlatPart;
+import edu.rice.cs.hpcviewer.ui.parts.TopDownPart;
 
 public class FileOpenDatabase 
 {
@@ -28,6 +32,11 @@ public class FileOpenDatabase
 		
 		if (experiment == null)
 			return;
+		
+		partService.showPart(FlatPart.ID, PartState.CREATE);
+		partService.showPart(BottomUpPart.ID, PartState.CREATE);
+		partService.showPart(TopDownPart.ID, PartState.VISIBLE);
+		partService.showPart(TopDownPart.ID, PartState.ACTIVATE);
 		
 		if (broker.post(ExperimentAddOn.EVENT_HPC_NEW_DATABASE, experiment)) {
 		}
--- edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/parts/BottomUpPart.java
+++ edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/parts/BottomUpPart.java
@@ -1,11 +1,103 @@
 package edu.rice.cs.hpcviewer.ui.parts;
 
-public class BottomUpPart 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.eclipse.e4.ui.model.application.ui.basic.MPart;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.e4.ui.workbench.modeling.IPartListener;
+import org.eclipse.swt.widgets.Composite;
+
+import edu.rice.cs.hpc.data.experiment.BaseExperiment;
+import edu.rice.cs.hpc.data.experiment.Experiment;
+import edu.rice.cs.hpc.data.experiment.scope.RootScope;
+import edu.rice.cs.hpc.data.experiment.scope.RootScopeType;
+import edu.rice.cs.hpcviewer.ui.internal.BaseContentViewer;
+import edu.rice.cs.hpcviewer.ui.parts.editor.ViewEventHandler;
+
+public class BottomUpPart implements IBaseView, IPartListener
 {
 	static public final String ID = "edu.rice.cs.hpcviewer.ui.part.bottomup";
 
+	@Inject EPartService partService;
+	@Inject IEventBroker broker;
+
+	private ViewEventHandler eventHandler;
+	private IContentViewer   contentViewer;
+
+	private Experiment experiment;
+
+	private RootScope root;
+
 	public BottomUpPart() {
-		// TODO Auto-generated constructor stub
+		root = null;
+		experiment = null;
+	}
+
+	@PostConstruct
+    public void createControls(Composite parent) {
+		
+		eventHandler = new ViewEventHandler(this, broker, partService);
+
+		contentViewer = new BaseContentViewer();
+    	contentViewer.createContent(parent);
+    	
+		partService.addPartListener(this);
+
+	}
+
+	@PreDestroy
+	public void preDestroy() {
+		eventHandler.dispose();
+		partService.removePartListener(this);
+	}
+
+	@Override
+	public void setExperiment(BaseExperiment experiment) {
+		root = null;
+		this.experiment = (Experiment) experiment;
+	}
+
+	@Override
+	public String getViewType() {
+		return Experiment.TITLE_BOTTOM_UP_VIEW;
+	}
+
+	@Override
+	public String getID() {
+		return ID;
+	}
+	
+
+	@Override
+	public void partActivated(MPart part) {}
+
+	@Override
+	public void partBroughtToTop(MPart part) {}
+
+	@Override
+	public void partDeactivated(MPart part) {}
+
+	@Override
+	public void partHidden(MPart part) {}
+
+	@Override
+	public void partVisible(MPart part) {
+		if (!part.getElementId().equals(ID))
+			return;
+		
+		if (experiment == null) return;
+		
+		if (root != null) return;
+		
+		RootScope rootCCT  = experiment.getRootScope(RootScopeType.CallingContextTree);
+		RootScope rootCall = experiment.getRootScope(RootScopeType.CallerTree);
+		
+		root = experiment.createCallersView(rootCCT, rootCall);
+		
+		contentViewer.setData(root);
 	}
 
 }
