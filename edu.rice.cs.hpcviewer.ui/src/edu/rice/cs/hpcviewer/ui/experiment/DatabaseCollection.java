74a43438e53c5667a65edd01ce5478d703922de4
Laksono Adhianto <laksono@gmail.com>
2020-06-10 12:01:24 -0500

    Refine the app start-app

--- edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/experiment/DatabaseCollection.java
+++ edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/experiment/DatabaseCollection.java
@@ -1,5 +1,8 @@
 package edu.rice.cs.hpcviewer.ui.experiment;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -7,25 +10,34 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileInfo;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.e4.core.di.annotations.Creatable;
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.core.services.events.IEventBroker;
+import org.eclipse.e4.ui.di.UIEventTopic;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
-import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
+import org.eclipse.e4.ui.workbench.IWorkbench;
+import org.eclipse.e4.ui.workbench.UIEvents;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService.PartState;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
 import edu.rice.cs.hpc.data.experiment.BaseExperiment;
 import edu.rice.cs.hpc.data.experiment.scope.RootScope;
 import edu.rice.cs.hpcviewer.ui.internal.ViewerDataEvent;
-import edu.rice.cs.hpcviewer.ui.parts.BaseViewPart;
 import edu.rice.cs.hpcviewer.ui.parts.BottomUpPart;
 import edu.rice.cs.hpcviewer.ui.parts.FlatPart;
 import edu.rice.cs.hpcviewer.ui.parts.IBaseView;
 import edu.rice.cs.hpcviewer.ui.parts.TopDownPart;
-import edu.rice.cs.hpcviewer.ui.parts.editor.Editor;
 
 /***
  * <b>
@@ -43,18 +55,68 @@ import edu.rice.cs.hpcviewer.ui.parts.editor.Editor;
 @Singleton
 public class DatabaseCollection 
 {
-	static final private String MAIN_WINDOW = "edu.rice.cs.hpcviewer.window.main";
-	
 	private ConcurrentLinkedQueue<BaseExperiment> queueExperiment;
 	private HashMap<BaseExperiment, ViewerDataEvent> mapColumnStatus;
 	
-	@Inject EPartService partService;
 	
 	public DatabaseCollection() {
 		queueExperiment = new ConcurrentLinkedQueue<>();
 		mapColumnStatus = new HashMap<BaseExperiment, ViewerDataEvent>();
 	}
 	
+	@Inject
+	@Optional
+	private void subscribeApplicationCompleted(
+			@UIEventTopic(UIEvents.UILifeCycle.APP_STARTUP_COMPLETE) final MApplication application,
+			final EPartService partService,
+			final IEventBroker broker,
+			final EModelService modelService,
+			final IWorkbench workbench) {
+		
+		
+		// handling the command line arguments:
+		// if one of the arguments specify a file or a directory,
+		// try to find the experiment.xml and load it.
+		
+		String args[] = Platform.getApplicationArgs();
+		
+		Display display = Display.getDefault();
+		Shell myShell   = display.getActiveShell();
+		
+		if (myShell == null) {
+			myShell = new Shell(SWT.TOOL | SWT.NO_TRIM);
+		}
+		BaseExperiment experiment    = null;
+		ExperimentManager expManager = new ExperimentManager();
+		
+		String path = null;
+		
+		for (String arg: args) {
+			if (arg.charAt(0) != '-')
+				path = arg;
+		}
+		if (path == null || path.length() < 1) {
+			experiment    = expManager.openFileExperiment(myShell);
+		} else {
+			experiment = openDatabase(myShell, expManager, args[0]);
+		}
+		if (experiment == null)
+			return;
+		
+		addDatabase(experiment, application, partService, broker, modelService);
+	}
+	
+	/****
+	 * Add a new database into the collection.
+	 * This database can be remove later on by calling {@code removeLast}
+	 * or {@code removeAll}.
+	 * 
+	 * @param experiment
+	 * @param application
+	 * @param service
+	 * @param broker
+	 * @param modelService
+	 */
 	public void addDatabase(BaseExperiment experiment, 
 			MApplication 	application, 
 			EPartService    service,
@@ -63,14 +125,6 @@ public class DatabaseCollection
 		
 		queueExperiment.add(experiment);
 		
-		/*
-		if (broker.post(ViewerDataEvent.TOPIC_HPC_NEW_DATABASE, experiment)) {
-			if (application != null && modelService != null) {
-				MWindow window = (MWindow) modelService.find(MAIN_WINDOW, application);
-				window.setLabel("hpcviewer - " + experiment.getDefaultDirectory().getPath());
-			}
-		} */
-		
 		if (service == null) {
 			System.out.println("Error: service is not available");
 			return;
@@ -78,9 +132,9 @@ public class DatabaseCollection
 		MPartStack stack = (MPartStack)modelService.find("edu.rice.cs.hpcviewer.ui.partstack.lower", application);
 		
 		final String []partIds = {
-				TopDownPart.IDdesc,
+				TopDownPart .IDdesc,
 				BottomUpPart.IDdesc,
-				FlatPart.IDdesc
+				FlatPart    .IDdesc
 		};
 		
 		Object []children = experiment.getRootScopeChildren();
@@ -89,7 +143,7 @@ public class DatabaseCollection
 
 			RootScope root = (RootScope) children[i];
 			
-			final MPart part = partService.createPart(partIds[i]);
+			final MPart part = service.createPart(partIds[i]);
 			
 			stack.getChildren().add(part);
 
@@ -97,7 +151,7 @@ public class DatabaseCollection
 			part.setElementId(experiment.getDefaultDirectory().getAbsolutePath() + ":" + root.getRootName());
 			
 			if (i==0) {
-				partService.showPart(part, PartState.VISIBLE);
+				service.showPart(part, PartState.VISIBLE);
 				
 				IBaseView view = (IBaseView) part.getObject();			
 				view.setExperiment(experiment);
@@ -140,5 +194,41 @@ public class DatabaseCollection
 	
 	public ViewerDataEvent getColumnStatus(BaseExperiment experiment) {
 		return mapColumnStatus.get(experiment);
+	}
+	
+	
+	/****
+	 * Find a database for a given path
+	 * 
+	 * @param shell the active shell
+	 * @param expManager the experiment manager
+	 * @param sPath path to the database
+	 * @return
+	 */
+	private BaseExperiment openDatabase(Shell shell, ExperimentManager expManager, String sPath) {
+    	IFileStore fileStore;
+
+		try {
+			fileStore = EFS.getLocalFileSystem().getStore(new URI(sPath));
+		} catch (URISyntaxException e) {
+			// somehow, URI may throw an exception for certain schemes. 
+			// in this case, let's do it traditional way
+			fileStore = EFS.getLocalFileSystem().getStore(new Path(sPath));
+			e.printStackTrace();
+		}
+    	IFileInfo objFileInfo = fileStore.fetchInfo();
+
+    	if (!objFileInfo.exists())
+    		return null;
+
+    	BaseExperiment experiment = null;
+    	
+    	if (objFileInfo.isDirectory()) {
+    		experiment = expManager.openDatabaseFromDirectory(shell, sPath);
+    	} else {
+			EFS.getLocalFileSystem().fromLocalFile(new File(sPath));
+			experiment = expManager.loadExperiment(shell, sPath);
+    	}
+    	return experiment;
 	}
 }
--- edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/handlers/LifeCycle.java
+++ edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/handlers/LifeCycle.java
@@ -1,20 +1,12 @@
 package edu.rice.cs.hpcviewer.ui.handlers;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 
 import javax.inject.Inject;
 import javax.inject.Named;
 
-import org.eclipse.core.filesystem.EFS;
-import org.eclipse.core.filesystem.IFileInfo;
-import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.services.IServiceConstants;
@@ -25,14 +17,11 @@ import org.eclipse.e4.ui.workbench.lifecycle.ProcessRemovals;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.jface.window.Window;
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 
-import edu.rice.cs.hpc.data.experiment.BaseExperiment;
 import edu.rice.cs.hpcviewer.ui.experiment.DatabaseCollection;
-import edu.rice.cs.hpcviewer.ui.experiment.ExperimentManager;
 import edu.rice.cs.hpcviewer.ui.resources.IconManager;
 
 public class LifeCycle 
@@ -64,35 +53,6 @@ public class LifeCycle
 			}
 		}
 		Window.setDefaultImages(listImages);
-		
-		// handling the command line arguments:
-		// if one of the arguments specify a file or a directory,
-		// try to find the experiment.xml and load it.
-		
-		String args[] = Platform.getApplicationArgs();
-		
-		Shell myShell = display.getActiveShell();
-		if (myShell == null) {
-			myShell = new Shell(SWT.TOOL | SWT.NO_TRIM);
-		}
-		BaseExperiment experiment    = null;
-		ExperimentManager expManager = new ExperimentManager();
-		
-		String path = null;
-		
-		for (String arg: args) {
-			if (arg.charAt(0) != '-')
-				path = arg;
-		}
-		if (path == null || path.length() < 1) {
-			experiment    = expManager.openFileExperiment(myShell);
-		} else {
-			experiment = openDatabase(myShell, expManager, args[0]);
-		}
-		if (experiment == null)
-			return;
-		
-		databaseCollection.addDatabase(experiment, null, partService, broker, modelService);
 	}
 	
 
@@ -107,39 +67,4 @@ public class LifeCycle
 	@ProcessRemovals
 	void processRemovals(IEclipseContext workbenchContext) {}
 
-	
-	/****
-	 * Find a database for a given path
-	 * 
-	 * @param shell the active shell
-	 * @param expManager the experiment manager
-	 * @param sPath path to the database
-	 * @return
-	 */
-	private BaseExperiment openDatabase(Shell shell, ExperimentManager expManager, String sPath) {
-    	IFileStore fileStore;
-
-		try {
-			fileStore = EFS.getLocalFileSystem().getStore(new URI(sPath));
-		} catch (URISyntaxException e) {
-			// somehow, URI may throw an exception for certain schemes. 
-			// in this case, let's do it traditional way
-			fileStore = EFS.getLocalFileSystem().getStore(new Path(sPath));
-			e.printStackTrace();
-		}
-    	IFileInfo objFileInfo = fileStore.fetchInfo();
-
-    	if (!objFileInfo.exists())
-    		return null;
-
-    	BaseExperiment experiment = null;
-    	
-    	if (objFileInfo.isDirectory()) {
-    		experiment = expManager.openDatabaseFromDirectory(shell, sPath);
-    	} else {
-			EFS.getLocalFileSystem().fromLocalFile(new File(sPath));
-			experiment = expManager.loadExperiment(shell, sPath);
-    	}
-    	return experiment;
-	}
 }
