318e2ea62460572e73cbe95c3bbe72c50c3807ae
Laksono Adhianto <laksono@gmail.com>
2020-06-26 18:58:22 -0500

    Integrating hpcfilter into hpcviewer.ui

--- edu.rice.cs.hpc.filter/src/edu/rice/cs/hpc/filter/action/ShowFilterView.java
+++ edu.rice.cs.hpc.filter/src/edu/rice/cs/hpc/filter/action/ShowFilterView.java
@@ -1,20 +1,23 @@
 package edu.rice.cs.hpc.filter.action;
 
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
+import javax.inject.Named;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.widgets.Shell;
 
 import edu.rice.cs.hpc.filter.view.FilterPropertyDialog;
 
-public class ShowFilterView extends AbstractHandler 
+public class ShowFilterView 
 {
 	final public static String ID = "edu.rice.cs.hpc.filter.action.ShowFilterView";
 
-	@Override 
-	public Object execute(ExecutionEvent event) throws ExecutionException 
+	@Execute
+	public Object execute( @Named(IServiceConstants.ACTIVE_SHELL) Shell shell, IEventBroker eventBroker) 
 	{
-		FilterPropertyDialog dialog = new FilterPropertyDialog(null);
+		FilterPropertyDialog dialog = new FilterPropertyDialog(shell);
 		
 		if (dialog.open() == IDialogConstants.OK_ID) {
 			
