88a601aafdd08311e9db5efb2aab033209a9e718
Laksono Adhianto <laksono@gmail.com>
2020-06-25 20:34:59 -0500

    Special expression to close database

---
+++ edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/expression/DatabaseCloseExpression.java
@@ -0,0 +1,19 @@
+ 
+package edu.rice.cs.hpcviewer.ui.expression;
+
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.di.annotations.Evaluate;
+
+import edu.rice.cs.hpcviewer.ui.experiment.DatabaseCollection;
+
+public class DatabaseCloseExpression 
+{
+	@Inject DatabaseCollection database;
+
+	@Evaluate
+	public boolean evaluate() {
+		
+		return database.getNumDatabase()>0;
+	}
+}
