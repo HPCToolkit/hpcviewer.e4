473a19df9e589743b8685d259df157bb0bc686c2
Laksono Adhianto <laksono@gmail.com>
2020-07-13 15:18:27 -0500

    Avoid closing hpctraceviewer window
    If we close it, we'll destroy it, and it's a nightmare to recreate it.

--- edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/handlers/ShowTrace.java
+++ edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/handlers/ShowTrace.java
@@ -8,7 +8,9 @@ import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.e4.ui.workbench.modeling.IWindowCloseHandler;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 
 import javax.inject.Named;
@@ -29,22 +31,41 @@ public class ShowTrace
 		
 		MPerspective perspective = (MPerspective) modelService.find(HPCTraceView.ID_PERSPECTIVE, application);
 		
-		if (perspective == null || winTrace == null)
-			return;
-		
-		int height = window.getHeight();
-		int width  = window.getWidth();
-		int x = window.getX() + 100;
-		int y = window.getY() + 100;
-		
-		winTrace.setX(x);
-		winTrace.setY(y);
-		winTrace.setHeight(height);
-		winTrace.setWidth(width);
+		if (perspective == null || winTrace == null) {
 
-		winTrace.setVisible(true);
+			return;
+		}
+		
+	 	if (!winTrace.isVisible()) {
+			int height = window.getHeight();
+			int width  = window.getWidth();
+			int x = window.getX() + 100;
+			int y = window.getY() + 100;
+			
+			winTrace.setX(x);
+			winTrace.setY(y);
+			winTrace.setHeight(height);
+			winTrace.setWidth(width);
+
+			winTrace.setVisible(true);
+		}
 		winTrace.setOnTop(true);
 		
+		IEclipseContext context = winTrace.getContext();
+		if (context == null)
+			return;
+		
+		IWindowCloseHandler closeHandler = context.get(IWindowCloseHandler.class);
+		
+		closeHandler = new IWindowCloseHandler() {
+			
+			@Override
+			public boolean close(MWindow window) {
+				window.setVisible(false);
+				return false;
+			}
+		};
+		context.set(IWindowCloseHandler.class, closeHandler);
 	}
 	
 	
