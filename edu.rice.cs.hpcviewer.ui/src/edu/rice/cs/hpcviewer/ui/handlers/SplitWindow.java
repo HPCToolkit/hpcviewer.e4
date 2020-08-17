5054612164b3d657bbf786c55c9adb0c3174298d
Laksono Adhianto <laksono@gmail.com>
2020-08-16 21:02:44 -0500

    Move SplitWindow method into a separate class
    Just in case other classes need this functionality

--- edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/handlers/SplitWindow.java
+++ edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/handlers/SplitWindow.java
@@ -4,16 +4,12 @@ import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.ui.MElementContainer;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
-import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 
-import edu.rice.cs.hpc.data.experiment.BaseExperiment;
-import edu.rice.cs.hpcbase.ui.IBasePart;
+import edu.rice.cs.hpcviewer.ui.actions.WindowAction;
 import edu.rice.cs.hpcviewer.ui.addon.DatabaseCollection;
-
-import java.util.List;
 
 import javax.inject.Named;
 
@@ -27,33 +23,7 @@ public class SplitWindow
 						 EPartService       partService,
 						 @Named(IServiceConstants.ACTIVE_PART) MPart part) {
 		
-		MElementContainer<MUIElement> container = part.getParent();
-		MElementContainer<MUIElement> parentContainer = container.getParent();
-		
-		MPartStack partStack = modelService.createModelElement(MPartStack.class);
-		parentContainer.getChildren().add(partStack);
-		
-		// move first the current active part
-		modelService.move(part, partStack);
-		
-		// move parts with the same database
-		IBasePart view = (IBasePart) part.getObject();
-		if (view == null)
-			return;
-		
-		BaseExperiment currentExperiment = view.getExperiment();
-		
-		List<MPart> list = modelService.findElements(container, null, MPart.class);
-		for(MPart otherPart: list) {
-			Object obj = otherPart.getObject();
-			if (obj != null && (obj instanceof IBasePart)) {
-				BaseExperiment experiment = ((IBasePart)obj).getExperiment();
-				if (experiment == currentExperiment) {
-					modelService.move(otherPart, partStack);
-				}
-			}
-		}
-		partService.activate(part);
+		WindowAction.SplitWindow(modelService, partService, part);
 	}
 	
 	
