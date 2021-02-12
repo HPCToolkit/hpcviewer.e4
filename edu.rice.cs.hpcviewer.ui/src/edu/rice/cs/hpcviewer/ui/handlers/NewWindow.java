a0d3430c6d1631ca95a5580c8c0a26b6b8f409e1
Laksono Adhianto <laksono@gmail.com>
2021-02-12 17:52:01 -0600

    Fix issue #7 open dialog after new window creation
    This is a hack to solve issue #7 by using Eclipse E4 temp API:
    ECommandService and EHandlerService. This API is used to execute a menu
    command programmatically, and there is no other way to do this without
    these temp API.

--- edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/handlers/NewWindow.java
+++ edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/handlers/NewWindow.java
@@ -2,8 +2,14 @@
 package edu.rice.cs.hpcviewer.ui.handlers;
 
 import javax.inject.Inject;
+
+import org.eclipse.core.commands.Command;
+import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.e4.core.commands.ECommandService;
+import org.eclipse.e4.core.commands.EHandlerService;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.model.application.MApplication;
+import org.eclipse.e4.ui.model.application.commands.MCommand;
 import org.eclipse.e4.ui.model.application.ui.basic.MTrimmedWindow;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 
@@ -12,6 +18,7 @@ import edu.rice.cs.hpcviewer.ui.addon.DatabaseCollection;
 public class NewWindow 
 {
 	static public final String ID_WINDOW = "edu.rice.cs.hpcviewer.window.main";
+	static private final String ID_OPEN_COMMAND = "edu.rice.cs.hpcviewer.ui.command.open";
 	
 	@Inject DatabaseCollection database;
 	
@@ -20,5 +27,13 @@ public class NewWindow
 		  MTrimmedWindow newWin = (MTrimmedWindow)modelService.cloneSnippet(app, "edu.rice.cs.hpcviewer.ui.trimmedwindow.main", null);
 
 		  app.getChildren().add(newWin);
+		  
+		  ECommandService cs = newWin.getContext().get(ECommandService.class);
+		  EHandlerService hs = newWin.getContext().get(EHandlerService.class);
+		  
+		  Command cmdOpen = cs.getCommand(ID_OPEN_COMMAND);
+		  ParameterizedCommand pc = new ParameterizedCommand(cmdOpen, null);
+		  
+		  hs.executeHandler(pc);
 	}
 }
