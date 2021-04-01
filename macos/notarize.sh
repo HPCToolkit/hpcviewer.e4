7960ec2bb1143016e8c991f727c8b2d77e50cab4
Laksono Adhianto <laksono@gmail.com>
2021-04-01 13:49:15 -0400

    refine notarization script

--- macos/notarize.sh
+++ macos/notarize.sh
@@ -1,25 +1,35 @@
 #!/bin/zsh
 
+if ! command -v gon &> /dev/null
+then 
+	echo "gon is not installed. Unable to continue the script"
+	echo "Please install gon from https://github.com/mitchellh/gon" 
+	exit
+fi
+
 check_file() {
 if [ ! -f $1 ]; then
-	die "hpcviewer mac file doesn't exist: $1"
+	echo "hpcviewer mac file doesn't exist: $1"
+	exit 1
 fi
 }
 
 FILE=`ls hpcviewer-*-macosx.cocoa.x86_64.zip`
 check_file $FILE
 
+FILE_BASE="${FILE%.zip}"
+echo "File base: ${FILE_BASE}"
+
 DIR_CONFIG="macos"
 BASE_CONFIG="config.hcl"
-FILE_CONFIG="${DIR_CONFIG}/${BASE_CONFIG}"
 FILE_PLIST="${DIR_CONFIG}/p.plist"
 
-check_file $FILE_CONFIG
 check_file $FILE_PLIST
 
 # check the AC_PASSWORD env
 if [[ -z "${AC_PASSWORD}" ]] ; then
-	die "AC_PASSWORD env is not set"
+	echo "AC_PASSWORD env is not set"
+	exit 1
 fi
 
 DIR_PREP="prepare_sign"
@@ -27,7 +37,27 @@ DIR_PREP="prepare_sign"
 rm -rf $DIR_PREP
 mkdir $DIR_PREP
 
-cp $FILE_CONFIG $DIR_PREP
+cat << EOF > $DIR_PREP/$BASE_CONFIG
+source = ["./hpcviewer.app"]
+bundle_id = "edu.rice.cs.hpcviewer"
+
+apple_id {
+  username = "la5@rice.edu"
+  password = "@env:AC_PASSWORD"
+}
+
+sign {
+  application_identity = "Developer ID Application: Laksono Adhianto"
+  entitlements_file = "p.plist"
+}
+
+dmg {
+  output_path = "${FILE_BASE}.dmg"
+  volume_name = "hpcviewer"
+}
+
+EOF
+
 cp $FILE_PLIST  $DIR_PREP
 
 cd $DIR_PREP
