fa8615470482fa2cc1ab791b8028cc59644b2f40
Laksono Adhianto <laksono@gmail.com>
2021-04-29 16:15:21 -0500

    Fix filter panel for multiple windows

--- edu.rice.cs.hpcfilter/src/edu/rice/cs/hpcfilter/action/ShowFilterView.java
+++ edu.rice.cs.hpcfilter/src/edu/rice/cs/hpcfilter/action/ShowFilterView.java
@@ -1,12 +1,14 @@
 package edu.rice.cs.hpcfilter.action;
 
+import java.util.Map;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.services.events.IEventBroker;
+import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.services.IServiceConstants;
-import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.swt.widgets.Shell;
 
 import edu.rice.cs.hpcfilter.service.FilterStateProvider;
@@ -19,13 +21,29 @@ public class ShowFilterView
 	@Inject FilterStateProvider filterService;
 
 	@Execute
-	public Object execute( @Named(IServiceConstants.ACTIVE_SHELL) Shell shell, IEventBroker eventBroker) 
+	public Object execute( @Named(IServiceConstants.ACTIVE_SHELL) Shell shell, 
+							IEventBroker eventBroker,
+							MApplication application) 
 	{
-		FilterPropertyDialog dialog = new FilterPropertyDialog(shell, filterService);
+		Map<String, Object> map = application.getTransientData();
+		Object obj = map.get(ID);
 		
-		if (dialog.open() == IDialogConstants.OK_ID) {
+		// if filter panel is already shown, even in other window, we should
+		// bring it to the top.
+		// we only allow one filter per application (or session)
+		
+		if (obj != null && (obj instanceof FilterPropertyDialog)) {
+			FilterPropertyDialog dlg = (FilterPropertyDialog) obj;
+			if (!dlg.getShell().isDisposed()) {
+				dlg.getShell().setActive();
+				return null;
+			}
 		}
-
+		FilterPropertyDialog dialog = new FilterPropertyDialog(shell, filterService);
+		map.put(ID, dialog);
+		
+		dialog.open();
+		
 		return null;
 	}
 }
