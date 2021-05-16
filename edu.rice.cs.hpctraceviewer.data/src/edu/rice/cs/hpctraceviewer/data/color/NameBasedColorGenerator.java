61ebf58fd7ab989058f5c50b4c9d6de11ee88962
Laksono Adhianto <laksono@gmail.com>
2021-05-16 14:25:19 -0500

    Draft using apache codec in hpctraceviewer.data

--- edu.rice.cs.hpctraceviewer.data/src/edu/rice/cs/hpctraceviewer/data/color/NameBasedColorGenerator.java
+++ edu.rice.cs.hpctraceviewer.data/src/edu/rice/cs/hpctraceviewer/data/color/NameBasedColorGenerator.java
@@ -1,5 +1,6 @@
 package edu.rice.cs.hpctraceviewer.data.color;
 
+import org.apache.commons.codec.digest.MurmurHash3;
 import org.eclipse.swt.graphics.RGB;
 
 
@@ -13,8 +14,15 @@ public class NameBasedColorGenerator implements IColorGenerator
 
 	@Override
 	public RGB createColor(String procedureName) {
-
-		int hash = procedureName.hashCode();
+		// use deprecated hash32 method because it's supported by
+		// both apache codec 1.13 and 1.15.
+		// Maven by default will download 1.13, while Eclipse downloads
+		// 1.15 version :-(
+		
+		int hash = MurmurHash3.hash32(procedureName.getBytes());
+		//int hash = procedureName.hashCode();
+		
+		// convert the lower 24 bits to color
 		int red  = (hash >> 16) & 0xFF;
 		int green = (hash >> 8) & 0xFF;
 		int blue  = hash & 0xFF;
