44b32c4fa3b3f2148097fdf4fc86be52a7679996
Laksono Adhianto <laksono@gmail.com>
2022-02-16 11:14:44 -0600

    Refine call site text painter

--- edu.rice.cs.hpctree/src/edu/rice/cs/hpctree/internal/CallSiteTextPainter.java
+++ edu.rice.cs.hpctree/src/edu/rice/cs/hpctree/internal/CallSiteTextPainter.java
@@ -8,21 +8,62 @@ import org.eclipse.nebula.widgets.nattable.style.DisplayMode;
 import org.eclipse.nebula.widgets.nattable.style.IStyle;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Rectangle;
+
 import edu.rice.cs.hpcdata.experiment.scope.CallSiteScope;
 import edu.rice.cs.hpcdata.experiment.scope.LineScope;
 import edu.rice.cs.hpcdata.experiment.scope.Scope;
 import edu.rice.cs.hpcsetting.color.ColorManager;
 import edu.rice.cs.hpctree.resources.ViewerColorManager;
 
+/**************************************************
+ * 
+ * Special painter for the line number of a call site
+ *
+ **************************************************/
 public class CallSiteTextPainter extends TextPainter 
 {
 	private final ScopeTreeDataProvider dataProvider;
 	private boolean enabled;
 	private DisplayMode displayMode;
 	
+	/****
+	 * Create a call site text painter given the data provider
+	 * @param dataProvider
+	 */
 	public CallSiteTextPainter(ScopeTreeDataProvider dataProvider) {
+		super();
 		this.dataProvider = dataProvider;
 		this.enabled = false;
+	}
+	
+	
+	@Override
+    public int getPreferredWidth(ILayerCell cell, GC gc, IConfigRegistry configRegistry) {
+		String text = convertDataType(cell, configRegistry);
+		if (text.equals(EMPTY))
+			return 0;
+		
+		return super.getPreferredWidth(cell, gc, configRegistry);
+	}
+	
+	
+	@Override
+    public int getPreferredHeight(ILayerCell cell, GC gc, IConfigRegistry configRegistry) {
+		String text = convertDataType(cell, configRegistry);
+		if (text.equals(EMPTY))
+			return 0;
+
+		return super.getPreferredHeight(cell, gc, configRegistry);
+	}
+	
+	@Override
+    public void paintCell(ILayerCell cell, GC gc, Rectangle rectangle, IConfigRegistry configRegistry) {
+		String text = convertDataType(cell, configRegistry);
+		if (text.equals(EMPTY))
+			return;
+		
+		super.paintCell(cell, gc, rectangle, configRegistry);
 	}
 	
 	@Override
