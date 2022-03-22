70e755eb165a4109f95fc20017a634e7fe3b09bb
Laksono Adhianto <laksono@gmail.com>
2022-03-21 19:21:43 -0500

    Add test for cct filter

---
+++ tests/edu.rice.cs.hpctest.viewer/src/edu/rice/cs/hpctest/viewer/FilterTest.java
@@ -0,0 +1,85 @@
+package edu.rice.cs.hpctest.viewer;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import edu.rice.cs.hpcdata.experiment.Experiment;
+import edu.rice.cs.hpcdata.filter.FilterAttribute;
+import edu.rice.cs.hpcfilter.service.FilterMap;
+
+
+@TestMethodOrder(OrderAnnotation.class)
+class FilterTest {
+	private static Experiment []experiments;
+	private static File []database;
+	private static String []dbPaths = new String[] {"bug-no-gpu-trace", "prof2" + File.separator + "loop-inline"};
+	
+	private static FilterMap fmap;
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+		experiments = new Experiment[dbPaths.length];
+		database   = new File[dbPaths.length];
+		int i=0;
+		for (String dbp: dbPaths) {
+			
+			Path resource = Paths.get("..", "resources", dbp);
+			database[i] = resource.toFile();
+			
+			assertNotNull(database);
+
+			experiments[i]= new Experiment();
+			try {
+				experiments[i].open(database[i], null, Experiment.ExperimentOpenFlag.TREE_CCT_ONLY);
+			} catch (Exception e) {
+				assertFalse(e.getMessage(), true);
+			}
+			
+			assertNotNull(experiments[i].getRootScope());
+			i++;
+		}
+	}
+	
+	
+
+	@Test
+	@Order(2)
+	void testFilter() {
+		final int []numFilters = new int[] {36, 0};
+		int i=0;
+		for(var exp: experiments) {
+			int res = exp.filter(fmap);
+			
+			assertTrue(res >= numFilters[i]);
+			i++;
+		}
+	}
+
+
+	@Test
+	@Order(1)
+	void testFilterInit() {
+		fmap = new FilterMap();
+		var fname = fmap.getFilename();
+		assertNotNull(fname);
+		
+		FilterAttribute fa = new FilterAttribute();
+		fa.enable = true;
+		fa.filterType = FilterAttribute.Type.Self_Only;
+		fmap.put("__GI___*", fa);
+		
+		assertTrue( fmap.size() == 1);
+	}
+
+}
