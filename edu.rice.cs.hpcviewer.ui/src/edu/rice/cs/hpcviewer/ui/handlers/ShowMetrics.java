4806e97ac997e03924e9276e387c4a220f9af191
Laksono Adhianto <laksono@gmail.com>
2022-08-10 10:18:23 -0500

    Remove duplicate code (reported by sonar source)

--- edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/handlers/ShowMetrics.java
+++ edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/handlers/ShowMetrics.java
@@ -3,13 +3,9 @@ package edu.rice.cs.hpcviewer.ui.handlers;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.services.events.IEventBroker;
-import org.eclipse.e4.ui.di.AboutToShow;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
-import org.eclipse.e4.ui.model.application.ui.menu.MDirectMenuItem;
-import org.eclipse.e4.ui.model.application.ui.menu.MMenuElement;
 import org.eclipse.e4.ui.services.IServiceConstants;
-import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.swt.widgets.Shell;
 import org.slf4j.Logger;
@@ -23,51 +19,16 @@ import edu.rice.cs.hpcviewer.ui.ProfilePart;
 import edu.rice.cs.hpcviewer.ui.addon.DatabaseCollection;
 import edu.rice.cs.hpcviewer.ui.internal.AbstractView;
 
-import java.util.List;
-
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.eclipse.e4.core.contexts.Active;
-import org.eclipse.e4.core.di.annotations.CanExecute;
 
-public class ShowMetrics 
+public class ShowMetrics
 {
-	static final private String ID_MENU_URI = "bundleclass://edu.rice.cs.hpcviewer.ui/" + ShowMetrics.class.getName();
-	
 	@Inject EPartService partService;
 	@Inject DatabaseCollection database;
-	
-	
-	
-	@AboutToShow
-	public void aboutToShow( List<MMenuElement> items, 
-							 EModelService modelService, 
-							 MWindow window ) {
-		if (!canExecute(window))
-			return;
 		
-		var iterator = database.getIterator(window);
-
-		while(iterator.hasNext()) {
-			Experiment exp = (Experiment) iterator.next();
-			
-			String path    = exp.getDefaultDirectory().getAbsolutePath();
-			String label   = path;
-			
-			if (exp.isMergedDatabase()) {
-				label = "[Merged] " + label;
-			}
-			MDirectMenuItem menu = modelService.createModelElement(MDirectMenuItem.class);
-			
-			menu.setElementId(path);
-			menu.setLabel(label);
-			menu.setContributionURI(ID_MENU_URI);
-			
-			items.add(menu);
-		}		
-	}
-	
 	
 	@Execute
 	public void execute(@Named(IServiceConstants.ACTIVE_PART) MPart part, 
@@ -82,7 +43,7 @@ public class ShowMetrics
 			return;
 
 		Object obj = part.getObject();
-		if (obj == null || (!(obj instanceof ProfilePart)))
+		if (!(obj instanceof ProfilePart))
 			return;
 
 		ProfilePart profilePart = (ProfilePart) obj;
@@ -100,11 +61,4 @@ public class ShowMetrics
 		
 		profilePart.addEditor(input);
 	}
-	
-	
-	@CanExecute
-	public boolean canExecute(MWindow window) {		
-		return database.getNumDatabase(window)>0;
-	}
-		
 }
