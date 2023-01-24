b8a866b8ca90b17a155e4035f98c63b19cb35f4e
Laksono Adhianto <laksono@gmail.com>
2023-01-24 17:43:00 -0600

    Fix issue #275 (missing last trace sample)
    The root cause is because the viewer doesn't calculate properly the last
    sample.

--- edu.rice.cs.hpcdata/src/edu/rice/cs/hpcdata/db/version4/FileDB4.java
+++ edu.rice.cs.hpcdata/src/edu/rice/cs/hpcdata/db/version4/FileDB4.java
@@ -25,6 +25,14 @@ import edu.rice.cs.hpcdata.experiment.scope.visitors.TraceScopeVisitor;
  ********************************************************************/
 public class FileDB4 implements IFileDB 
 {
+	/** this constant is taken from prof-lean/formats/tracedb.h
+	 <pre>
+	  enum { FMT_TRACEDB_SZ_CtxSample = 0x0c }
+	  </pre>
+	 * 
+	 */
+	private static final int FMT_TRACEDB_SZ_CONTEXT_SAMPLE = 0x0c;
+	
 	private final DataTrace   dataTrace;
 	private final DataSummary dataSummary;
 	private final IExperiment experiment;
@@ -148,7 +156,7 @@ public class FileDB4 implements IFileDB
 		List<IdTuple> listId = dataSummary.getIdTuple();
 		int profile = listId.get(rank).getProfileIndex() - 1;
 		
-		return dataTrace.getOffset(profile) + dataTrace.getLength(profile) - dataTrace.getRecordSize();
+		return dataTrace.getEndOffset(profile) - FMT_TRACEDB_SZ_CONTEXT_SAMPLE;
 	}
 
 	@Override
