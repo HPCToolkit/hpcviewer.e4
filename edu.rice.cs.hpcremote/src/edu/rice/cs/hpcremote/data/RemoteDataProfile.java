aef8033da94b09757716811f5d2e3b11efb44dc5
Laksono Adhianto <laksono@gmail.com>
2023-08-31 08:53:30 -0500

    Draft loading remote database and convert to "local" objects

---
+++ edu.rice.cs.hpcremote/src/edu/rice/cs/hpcremote/data/RemoteDataProfile.java
@@ -0,0 +1,93 @@
+package edu.rice.cs.hpcremote.data;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hpctoolkit.client_server_common.calling_context.CallingContextId;
+import org.hpctoolkit.client_server_common.profile.ProfileId;
+import org.hpctoolkit.hpcclient.v1_0.HpcClient;
+import org.hpctoolkit.hpcclient.v1_0.UnknownProfileIdException;
+
+import edu.rice.cs.hpcdata.db.IdTuple;
+import edu.rice.cs.hpcdata.db.IdTupleType;
+import edu.rice.cs.hpcdata.db.version4.AbstractDataProfile;
+import edu.rice.cs.hpcdata.db.version4.IDataProfile;
+import edu.rice.cs.hpcdata.experiment.metric.MetricValueSparse;
+import io.vavr.collection.HashSet;
+import io.vavr.collection.Set;
+
+
+/*************************************************************
+ * 
+ * The implementation of {@code IDataProfile} for remote database
+ * <p>
+ * This class has exactly the same functionality as {@code DataSummary}, 
+ * the implementation for local database but can remotely access remote
+ * {@code profile.db} file and grab the metrics on the fly.
+ * </p>
+ * @see edu.rice.cs.hpcdata.db.version4.DataSummary
+ * 
+ *************************************************************/
+public class RemoteDataProfile extends AbstractDataProfile implements IDataProfile
+{
+	private final HpcClient client;
+
+	
+	/*****
+	 * Create an instance of remote access to {@code profile.db} file.
+	 * 
+	 * @param client
+	 * 			An object of {@code HpcClient} to access remote database
+	 * @param idTupleTypes
+	 * 			DataMeta's types of id tuples
+	 */
+	public RemoteDataProfile(HpcClient client, IdTupleType idTupleTypes) {
+		super(idTupleTypes);
+		this.client = client;
+	}
+	
+	
+	@Override
+	public void open(final String file) {
+		throw new IllegalAccessError("The method is cannot be used for opening remote database");
+	}
+	
+
+	@Override
+	public double getMetric(IdTuple idtuple, int cctId, int metricId) throws IOException {
+		var listMetrics = getMetrics(idtuple, cctId);
+		var value = listMetrics.stream().filter(mvs -> mvs.getIndex() == metricId).findAny();
+		if (value.isPresent())
+			return value.get().getValue();
+
+		return 0;
+	}
+
+	@Override
+	public List<MetricValueSparse> getMetrics(IdTuple idtuple, int cctId) throws IOException {
+		var profId = ProfileId.make(idtuple.getProfileIndex());
+		var nodeId = CallingContextId.make(cctId);
+		
+		Set<CallingContextId> setNodeId = HashSet.empty();
+		setNodeId.add(nodeId);
+		
+		try {
+			var metrics = client.getMetrics(profId, setNodeId);
+			List<MetricValueSparse> listMetrics = new ArrayList<>(metrics.size());
+
+			var iterator = metrics.iterator();
+			while(iterator.hasNext()) {
+				var item = iterator.next();
+				item._2.forEach((k, v) -> {
+					MetricValueSparse mvs = new MetricValueSparse(k.toShort(), v.getValue());
+					listMetrics.add(mvs);
+				});
+			}
+			return listMetrics;
+			
+		} catch (IOException | InterruptedException | UnknownProfileIdException e) {
+			throw new IllegalArgumentException("Unknown profile " + idtuple.toString() + " or cct " + cctId);
+		}		
+	}
+}
--- edu.rice.cs.hpcremote/src/edu/rice/cs/hpcremote/ui/OpenRemoteDatabase.java
+++ edu.rice.cs.hpcremote/src/edu/rice/cs/hpcremote/ui/OpenRemoteDatabase.java
@@ -3,6 +3,8 @@ package edu.rice.cs.hpcremote.ui;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Named;
 
@@ -11,9 +13,15 @@ import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Shell;
+import org.hpctoolkit.hpcclient.v1_0.HpcClient;
 
+import edu.rice.cs.hpcdata.db.IdTuple;
 import edu.rice.cs.hpcdata.db.version4.DataMeta;
+import edu.rice.cs.hpcdata.db.version4.IDataProfile;
 import edu.rice.cs.hpcdata.experiment.Experiment;
+
+import edu.rice.cs.hpcremote.data.RemoteDataProfile;
+import io.vavr.collection.Set;
 
 public class OpenRemoteDatabase {
 	@Execute
@@ -30,22 +38,18 @@ public class OpenRemoteDatabase {
 			return;
 		
 		try {
-			var metaDBbytes   = client.getMetaDbFileContents();
-			ByteBuffer buffer = ByteBuffer.wrap(metaDBbytes);
+			var dataMeta = collectMetaData(client);
+			var dataProfile = collectProfileData(client, dataMeta);
 			
-			DataMeta dataMeta = new DataMeta();
-			dataMeta.open(buffer);
-			
-			var idtuples = client.getHierarchicalIdentifierTuples();
 			var metrics  = client.getMetricsDefaultYamlContents();
 			
 			Experiment experiment = (Experiment) dataMeta.getExperiment();
 			
-			var str = String.format("Connection info: %s%nName: %s%nmeta.db length: %d%nId-tuples size: %d%nmetrics length: %d%n",
+			var str = String.format("Connection info: %s%nName: %s%nmeta.db load-modules: %d%nId-tuples size: %d%nmetrics length: %d%n",
 					remoteInfo.getId(),
 					experiment.getName(),
-					metaDBbytes.length,
-					idtuples.size(),
+					dataMeta.getNumLoadModules(),
+					dataProfile.getIdTuple().size(),
 					metrics.length);
 			
 			MessageDialog.openInformation(shell, "Connection succeeds", str);
@@ -56,8 +60,29 @@ public class OpenRemoteDatabase {
 					"Error message: " + e.getLocalizedMessage());
 		} catch (Exception e) {
 			MessageDialog.openError(shell, "Unknown error", e.getMessage());
-		}
-		
+		}		
 	}
+	
+	
+	private DataMeta collectMetaData(HpcClient client) throws IOException, InterruptedException {
+		var metaDBbytes   = client.getMetaDbFileContents();
+		ByteBuffer buffer = ByteBuffer.wrap(metaDBbytes);
 		
+		DataMeta dataMeta = new DataMeta();
+		dataMeta.open(buffer);
+		
+		return dataMeta;
+	}
+	
+	
+	private IDataProfile collectProfileData(HpcClient client, DataMeta dataMeta) throws IOException, InterruptedException {
+		Set<IdTuple> idtuples = client.getHierarchicalIdentifierTuples();
+		List<IdTuple> list = new ArrayList<>(idtuples.size());
+		idtuples.forEach(list::add);
+		
+		var dataProfile = new RemoteDataProfile(client, dataMeta.getExperiment().getIdTupleType());
+		dataProfile.setIdTuple(list);
+		
+		return dataProfile;
+	}
 }
