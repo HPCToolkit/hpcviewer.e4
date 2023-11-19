136a5f71b9add1bfdad40aa7ee3da65610f1a15d
Laksono Adhianto <laksono@gmail.com>
2023-11-19 10:01:31 -0600

    Issue #342: reorganize the interface and the class implementation

---
+++ edu.rice.cs.hpctraceviewer.filter/src/edu/rice/cs/hpctraceviewer/filter/ITraceFilter.java
@@ -0,0 +1,41 @@
+package edu.rice.cs.hpctraceviewer.filter;
+
+import java.util.List;
+
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Shell;
+
+import edu.rice.cs.hpcbase.IFilteredData;
+import edu.rice.cs.hpctraceviewer.data.SpaceTimeDataController;
+
+public interface ITraceFilter 
+{
+	IFilteredData filterTrace(SpaceTimeDataController stdc);
+
+	static IFilteredData filter(final Shell shell, final SpaceTimeDataController stdc) {
+		return new ITraceFilter() {
+			
+			@Override
+			public IFilteredData filterTrace(SpaceTimeDataController stdc) {
+				var dialog = new TraceFilterDialog(shell, stdc.getBaseData());
+				if (dialog.open() == Window.CANCEL)
+					return null;
+		        
+				List<Integer> listChecked = dialog.getCheckedIndexes();
+				
+				if (listChecked != null && !listChecked.isEmpty()){
+					
+					// update the data and broadcast to everyone that we probably have new filtered ranks
+					var filteredBaseData = stdc.getBaseData();
+					filteredBaseData.setIncludeIndex(listChecked);
+					
+					// hack: to update the process interval based on the new filtered ranks
+					stdc.setBaseData(filteredBaseData);
+
+					return filteredBaseData;
+				}
+				return null;
+			}
+		}.filterTrace(stdc);
+	}
+}
--- edu.rice.cs.hpctraceviewer.ui/src/edu/rice/cs/hpctraceviewer/ui/handlers/FilterRanks.java
+++ edu.rice.cs.hpctraceviewer.ui/src/edu/rice/cs/hpctraceviewer/ui/handlers/FilterRanks.java
@@ -6,19 +6,13 @@ import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.services.IServiceConstants;
-import org.eclipse.jface.window.Window;
 import org.eclipse.swt.widgets.Shell;
 
-import edu.rice.cs.hpcdata.db.IdTuple;
-import edu.rice.cs.hpcbase.IFilteredData;
-import edu.rice.cs.hpcdata.db.IFileDB.IdTupleOption;
 import edu.rice.cs.hpctraceviewer.data.SpaceTimeDataController;
-import edu.rice.cs.hpctraceviewer.filter.TraceFilterDialog;
+import edu.rice.cs.hpctraceviewer.filter.ITraceFilter;
 import edu.rice.cs.hpctraceviewer.ui.base.ITracePart;
 import edu.rice.cs.hpctraceviewer.ui.internal.TraceEventData;
 import edu.rice.cs.hpctraceviewer.ui.util.IConstants;
-
-import java.util.List;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -42,66 +36,14 @@ public class FilterRanks
 		ITracePart tracePart = (ITracePart) obj;
 		SpaceTimeDataController data = tracePart.getDataController();
 
-		var dialog = new TraceFilterDialog(shell, data.getBaseData());
-		if (dialog.open() == Window.CANCEL)
-			return;
-        
-		List<Integer> listChecked = dialog.getCheckedIndexes();
+		var filteredBaseData = ITraceFilter.filter(shell, data);
 		
-		if (listChecked != null && !listChecked.isEmpty()){
-			
-			// update the data and broadcast to everyone that we probably have new filtered ranks
-			// TODO: we need to check if the new one is the same with the old one or not.
-
-			var filteredBaseData = data.getBaseData();
-			filteredBaseData.setIncludeIndex(listChecked);
-			
-			// hack: to update the process interval based on the new filtered ranks
-			data.setBaseData(filteredBaseData);
-
+		if (filteredBaseData != null){
 			TraceEventData eventData = new TraceEventData(data, tracePart, filteredBaseData);
 			eventBroker.post(IConstants.TOPIC_FILTER_RANKS, eventData);
 		}
 	}
 	
-	
-	protected String[] getListOfItems(IFilteredData filteredBaseData) {
-
-        var mapToSamples = filteredBaseData.getMapFromExecutionContextToNumberOfTraces();
-        
-        List<IdTuple> listDenseIds = filteredBaseData.getDenseListIdTuple(IdTupleOption.BRIEF);
-        List<IdTuple> listIds = filteredBaseData.getListOfIdTuples(IdTupleOption.BRIEF);
-        
-        String []items    = new String[listDenseIds.size()];
-        boolean []checked = new boolean[listDenseIds.size()];
-
-        // initialize the ranks to be displayed in the dialog box
-        // to know which ranks are already shown, we need to compare with the filtered ranks.
-        // if the id tuple in the original list is the same as the one in filtered list, 
-        // them the rank is already displayed.
-        // This list needs to be optimized.
-        
-        for(int i=0, j=0; i<listDenseIds.size(); i++) {
-        	var idt = listDenseIds.get(i);
-        	int numSamples = 0;
-        	
-        	if(mapToSamples != null) {
-            	var samples = mapToSamples.get(idt);
-            	if (samples != null)
-            		numSamples = samples.intValue();
-        	}
-        	
-        	items[i] = idt.toString(filteredBaseData.getIdTupleTypes()) + (numSamples > 0 ? " (" + numSamples + " samples)" : "");
-        	
-        	if (j<listIds.size() && listDenseIds.get(i) == listIds.get(j)) {
-        		checked[i] = true;
-        		j++;
-        	} else {
-        		checked[i] = false;
-        	}
-        }
-        return items;
-	}
 	
 	@CanExecute
 	@Inject
@@ -116,5 +58,4 @@ public class FilterRanks
 
 		return ((ITracePart)obj).getInput() != null;
 	}
-		
 }
