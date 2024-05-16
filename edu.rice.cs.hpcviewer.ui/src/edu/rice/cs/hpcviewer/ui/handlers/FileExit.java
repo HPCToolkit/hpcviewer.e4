88c83ab2e8dfa6a643aa764732927efc21441467
Laksono Adhianto <laksono@gmail.com>
2024-05-16 09:38:34 -0500

    Force to close the remote connection in Exit menu
    Somehow window close registration is not called in Release mode (but
    works in Debug mode).
    Try to close the connection whenever users click the Exit menu

--- edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/handlers/FileExit.java
+++ edu.rice.cs.hpcviewer.ui/src/edu/rice/cs/hpcviewer/ui/handlers/FileExit.java
@@ -1,12 +1,33 @@
 package edu.rice.cs.hpcviewer.ui.handlers;
 
+
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.workbench.IWorkbench;
+import org.eclipse.e4.ui.workbench.modeling.EModelService;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.swt.widgets.Shell;
+
+import edu.rice.cs.hpcremote.ICollectionOfConnections;
+import edu.rice.cs.hpcviewer.ui.addon.DatabaseCollection;
 
 public class FileExit 
-{
+{	
 	@Execute
-	public void execute(IWorkbench workbench) {
+	public void execute(
+			IWorkbench workbench, 
+			@Optional DatabaseCollection databaseCollection,
+			@Optional EModelService modelService,
+			@Optional EPartService  partService) {
+		
+		var application = workbench.getApplication();
+		if (application != null && application.getSelectedElement() != null) {
+			var window = application.getSelectedElement();
+			if (databaseCollection != null) {
+				databaseCollection.removeAllDatabases(window, modelService, partService);
+			}
+			ICollectionOfConnections.disconnectAll((Shell) window.getWidget());
+		}
 		workbench.close();
 	}
 }
