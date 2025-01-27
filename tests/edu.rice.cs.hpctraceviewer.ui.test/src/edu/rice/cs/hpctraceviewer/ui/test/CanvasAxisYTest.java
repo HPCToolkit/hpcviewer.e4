eb06a35f7963368f736b04dc6c38a87b69ff9146
Laksono Adhianto <laksono@gmail.com>
2025-01-27 15:47:17 +0000

    Fix issue #394: portable unit test for both Retina and standard displays
    This was squashed from 'fix-issue-394'
    For details see https://gitlab.com/hpctoolkit/hpcviewer/-/merge_requests/361

--- tests/edu.rice.cs.hpctraceviewer.ui.test/src/edu/rice/cs/hpctraceviewer/ui/test/CanvasAxisYTest.java
+++ tests/edu.rice.cs.hpctraceviewer.ui.test/src/edu/rice/cs/hpctraceviewer/ui/test/CanvasAxisYTest.java
@@ -12,8 +12,10 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Event;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -29,19 +31,32 @@ import edu.rice.cs.hpctraceviewer.ui.test.internal.TraceTestCase;
  */
 public class CanvasAxisYTest extends TraceTestCase 
 {
+	
+	@Before
+	@Override
+	public void setUp() {
+		super.setUp();
+		
+		shell.setLayout(new FillLayout());
+
+		final var title = "test-canvas";
+		shell.setText(title);
+	}
 
 	@Test
 	public void testSetDataObject() throws Exception {
-		final var title = "test-canvas";
-		shell.setText(title);
-		shell.open();
 
 		var tracePart = getTracePart();
 		CanvasAxisY canvas = new CanvasAxisY(tracePart, shell);
+		canvas.setLayout(new FillLayout());
+		canvas.layout();
+
+		shell.layout();
+		shell.open();
 		
 		var bounds = shell.getBounds();
 		canvas.setBounds(bounds);
-		shell.pack();
+
 
 		var rec = canvas.getBounds();
 		assertNotNull(rec);
@@ -49,7 +64,7 @@ public class CanvasAxisYTest extends TraceTestCase
 		var experiments = TestDatabase.getExperiments();
 		
 		// test the y-axis canvas for all databases in the test directory
-		for(var exp: experiments) {
+		for (var exp: experiments) {
 			var stdc = createSTDC(exp, rec.height, rec.width);
 			
 			canvas.setData(stdc);
@@ -63,6 +78,10 @@ public class CanvasAxisYTest extends TraceTestCase
 			OperationHistoryEvent event = new OperationHistoryEvent(OperationHistoryEvent.DONE, history, op);
 			
 			canvas.historyNotification(event);
+
+			// show the window for debugging purpose and human test
+			// we don't need this for CI
+			showWindow(1000);
 			
 			// check the drawn pixels
 			Image img = new Image(canvas.getDisplay(), canvas.getBounds());
@@ -79,7 +98,7 @@ public class CanvasAxisYTest extends TraceTestCase
 			// if we have blue, yellow, blue, then the number of changes is 3
 			
 			for(int i=0; i<imgData.height; i++) {
-				int pixel = imgData.getPixel(5, imgData.y + i);
+				int pixel = imgData.getPixel(i, imgData.y + i);
 				var color = imgData.palette.getRGB(pixel);
 				
 				// make sure we start with the first color
@@ -87,23 +106,37 @@ public class CanvasAxisYTest extends TraceTestCase
 					oldColor = color;
 				} else if (!oldColor.equals(color)) {
 					numChanges++;
-					assertFalse(numChanges > numTraces);
 					oldColor = color;
-					
-					var mouse = new Event();
-					mouse.x = 6;
-					mouse.y = imgData.y + i;
-					canvas.notifyListeners(SWT.MouseHover, mouse);
 				}
 			}
+			
+			// random mouse hover to trigger a tooltip
+			var mouse = new Event();
+			mouse.x = imgData.x + imgData.width  / 4;
+			mouse.y = imgData.y + imgData.height / 4;
+
+			canvas.notifyListeners(SWT.MouseHover, mouse);
+
 			// make sure we free resources even for the test
 			// should make this automatically done
 			img.dispose();
 			gc.dispose();
 			
-			// number of alternate colors can't be bigger than number of id tuples
-			// this condition should work in all cases
-			assertTrue(numChanges > 0 && numChanges <= numTraces);
+			// In theory the number of alternate colors can't be bigger than number of id tuples
+			// However this is not the case for HD display like retina screen.
+			// So the safest way is to assume the number of changes is positive
+			if (numTraces > 0) {
+				assertTrue(numChanges > 0);
+				
+				// for retina display, one software pixel = 2 hardware pixels,
+				// for ultra hd perhaps one software pixel = 4 hardware pixels (idk, need to check)
+				// The number of changes can't be bigger than quarter of the traces even for normal display
+				//
+				// In the future we have to certain the pixel density without using SWT internal API
+				// At the moment it's okay with the constant number 4 which will fail when one software pixel
+				//   equals six or eigth hardware pixels on some displays in the future
+				assertFalse("changes: " + numChanges + ", traces: " + numTraces, numChanges < numTraces/4);
+			}
 		}
 		canvas.dispose();
 	}
